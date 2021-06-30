3cf9bcef8957174bca64b437601b74a8d84eb088
Eike Sebode <esebode@posteo.net>
2021-06-30 07:10:48 +0200

    some minor changes

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -7,6 +7,7 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Set monitors
+
 for m in $(polybar --list-monitors | cut -d":" -f1); do
   MONITOR=$m polybar --reload main &
 done
